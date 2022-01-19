16640d49a5e3b3dc06a6782e52e88df966eb80b9
Safnaj <safnaj99@gmail.com>
2022-01-20 01:09:36 +0530

    eslint added

--- src/App.js
+++ src/App.js
@@ -5,21 +5,17 @@ import Home from './pages/Home/Home';
 import Employees from './pages/Employees/Employees';
 
 function App() {
-      // console.log('App.js is running');
-      return (
-            <div>
-                  <BrowserRouter>
-                        <Navbar />
-                        <Routes>
-                              <Route path='/home' element={<Home />} />
-                              <Route
-                                    path='/employees'
-                                    element={<Employees />}
-                              />
-                        </Routes>
-                  </BrowserRouter>
-            </div>
-      );
+   // console.log('App.js is running');
+   return (
+      <div>
+         <BrowserRouter>
+            <Navbar />
+            <Routes>
+               <Route path='/home' element={<Home />} />
+               <Route path='/employees' element={<Employees />} />
+            </Routes>
+         </BrowserRouter>
+      </div>
+   );
 }
-
 export default App;
