b487949ede36a1de2cd92343e3c04401bdc2e19b
Safnaj <safnaj99@gmail.com>
2022-01-20 01:08:34 +0530

    eslint added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home/Home'
-import Employees from './pages/Employees/Employees'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home/Home';
+import Employees from './pages/Employees/Employees';
 
 function App() {
-   // console.log('App.js is running');
-   return (
-      <div>
-         <BrowserRouter>
-            <Navbar />
-            <Routes>
-               <Route path='/home' element={<Home />} />
-               <Route path='/employees' element={<Employees />} />
-            </Routes>
-         </BrowserRouter>
-      </div>
-   )
+      // console.log('App.js is running');
+      return (
+            <div>
+                  <BrowserRouter>
+                        <Navbar />
+                        <Routes>
+                              <Route path='/home' element={<Home />} />
+                              <Route
+                                    path='/employees'
+                                    element={<Employees />}
+                              />
+                        </Routes>
+                  </BrowserRouter>
+            </div>
+      );
 }
 
-export default App
+export default App;
